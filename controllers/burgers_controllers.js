6e3bec0c2c1939495d565180f006c59da304e6e9
smoot <smoothtacomann@gmail.com>
2018-12-03 21:38:20 -0500

    another update

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -7,7 +7,7 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
-    burgers.all(function (data) {
+    burger.all(function (data) {
         var hbsObject = {
             burger: data
         };
--- server.js
+++ server.js
@@ -7,14 +7,19 @@ var PORT = process.env.PORT || 3030;
 app.use(express.static("public"));
 
 // Parse application body as JSON
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({
+    extended: true
+}))
 
 app.use(express.json())
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+
 app.set("view engine", "handlebars");
 
 
@@ -23,7 +28,7 @@ var routes = require("./controllers/burgers_controllers.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
 });
